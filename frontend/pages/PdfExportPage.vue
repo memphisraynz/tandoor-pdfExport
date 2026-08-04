5d6feddb17a35e964a0edc004f9f139ecb8b82ef
memphisraynz <daniel.rayner@gmail.com>
2026-08-04 21:23:27 +1000

    Widen the PDF Export settings column
    It was scoped (cols=12 md=8 lg=6) back when this page was just a
    recipe picker - on a wide viewport that's roughly half the available
    content width, which looked broken once the appearance form grew to
    several fields. Widened to lg=9 xl=7.

--- frontend/pages/PdfExportPage.vue
+++ frontend/pages/PdfExportPage.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-row>
-            <v-col cols="12" md="8" lg="6">
+            <v-col cols="12" lg="9" xl="7">
                 <h1 class="mb-4">{{ $t('Export_Recipe_PDF') }}</h1>
                 <p class="text-medium-emphasis mb-4">{{ $t('Export_Recipe_PDF_Help') }}</p>
 
